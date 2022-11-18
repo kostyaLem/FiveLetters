849aa904bc40e46e31a29ae187de55104f19260c
kostyaLem <kostyaLem@yandex.ru>
2022-11-18 17:52:59 +0400

    Fix border color when letter changed state

---
+++ FiveLetters.UI/Converters/CanClearBorderConverter.cs
@@ -0,0 +1,28 @@
+﻿using FiveLetters.UI.Models;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FiveLetters.UI.Converters;
+
+public class CanClearBorderConverter : MarkupExtension, IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is CellStyle cellStyle)
+        {
+            return cellStyle != CellStyle.Empty;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+        => this;
+}
